d5c5b22771760ba5f8df8b74c8e60ece10e7e0ef
EmilyDSarani <emily.sarani@gmail.com>
2021-08-11 19:51:55 -0500

    instead of names, i did funny quotes with the name of the animal in them

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const h1El = document.getElementById('animal-quote')
+
 
 const dogEl = document.getElementById('doggo-button');
 const dogSound = document.getElementById('doggo-sound');
 
 dogEl.addEventListener('click', () => {
     dogSound.play();
+    h1El.textContent = 'Outside of a dog, a book is man\'s best friend. Inside of a dog it\'s too dark to read';
 });
 
 const catEl = document.getElementById('kitty-button');
@@ -11,6 +14,7 @@ const catSound = document.getElementById('kitty-sound');
 
 catEl.addEventListener('click', () => {
     catSound.play();
+    h1El.textContent = 'Cats have it all – admiration, endless sleep, and company only when they want it';
 });
 
 const horseEl = document.getElementById('horsey-button');
@@ -18,5 +22,7 @@ const horseSound = document.getElementById('horsey-sound');
 
 horseEl.addEventListener('click', () => {
     horseSound.play();
+    h1El.textContent = 'It\'s hard to lead a cavalry charge if you think you look funny on a horse';
 });
+
 
