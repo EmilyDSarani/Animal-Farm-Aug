690766713c265c88474b17bd20a07ea0f01b6e56
EmilyDSarani <emily.sarani@gmail.com>
2021-08-11 19:24:50 -0500

    restarted from step one, sound works!

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 
-const button = document.getElementById('dog-image');
-const dogSound = document.getElementById('dog-sound');
+const button = document.getElementById('doggo-button');
+const dogSound = document.getElementById('doggo-sound');
 
 button.addEventListener('click', () => {
     dogSound.play();
-});
-
-const button = document.getElementById('cat-image');
-const catSound = document.getElementById('cat-sound');
-
-button.addEventListener('click', () => {
-    catSound.play();
-});
-
-const button = document.getElementById('horse-image');
-const horseSound = document.getElementById('horse-sound');
-
-button.addEventListener('click', () => {
-    horseSound.play();
 });
